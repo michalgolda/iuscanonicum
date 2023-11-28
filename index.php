b0e1f86a7c63b885802c600bcd5ac2b1d3b0ce37
Michał Gołda <michal.golda@hotmail.com>
2023-11-28 20:38:38 +0100

    feat: link fonts

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <?php wp_head(); ?>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://api.fontshare.com/v2/css?f[]=amulya@400,500&display=swap" rel="stylesheet">
 </head>
 
 <body>
