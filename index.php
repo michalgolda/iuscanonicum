57f8d6522c892c09bc01b304ca10d46782686199
Michał Gołda <michal.golda@hotmail.com>
2023-12-05 19:43:07 +0100

    feat: add welcome section

--- index.php
+++ index.php
@@ -34,6 +34,54 @@
         <a class="scroll-down" href="#">
             <img src="<?php echo iuscanonicum_get_image_src('scroll-down-arrow.svg'); ?>" alt="scroll down arrow" />
         </a>
+        <section id="welcome" class="section">
+            <h2 class="section__heading">Witamy w naszej kancelarii</h2>
+            <div class="welcome__container">
+                <div class="container">
+                    <p>Duis felis nisi, vulputate quis accumsan non, finibus ac sapien. Etiam pharetra faucibus quam, ac auctor turpis commodo vitae. Integer condimentum ex eget est cursus porta. Vivamus posuere ante in lobortis tristique. Mauris ipsum dui, aliquam at dapibus quis, sagittis et dui. Sed pharetra elit arcu, nec luctus purus ultricies id. Nam imperdiet semper bibendum. Aenean bibendum ex sed ligula ultricies semper. Ut ante velit, vestibulum id elementum quis, mollis vitae nulla. Suspendisse potenti. In lorem turpis, auctor vel ultricies at, posuere id ipsum. Vestibulum vulputate ipsum arcu, sed laoreet metus efficitur in.</p>
+                </div>
+            </div>
+            <div class="contact-block">
+                <div class="contact-block__content container">
+                    <div class="contact-block__box">
+                        <div class="icon">
+                            <img class="icon__img" src="<?php echo iuscanonicum_get_image_src("email-icon.svg") ?>" alt="e-mail icon" />
+                        </div>
+                        <div class="contact-block__details">
+                            <h4>E-mail</h4>
+                            <p>kontakt@iuasnanonicum.pl</p>
+                        </div>
+                    </div>
+                    <div class="contact-block__box">
+                        <div class="icon">
+                            <img class="icon__img" src="<?php echo iuscanonicum_get_image_src("location-icon.svg") ?>" alt="location icon" />
+                        </div>
+                        <div class="contact-block__details">
+                            <h4>Lokalizacja</h4>
+                            <p>al. Beliny Prażmowskiego 9/331-514 Kraków</p>
+                        </div>
+                    </div>
+                    <div class="contact-block__box">
+                        <div class="icon">
+                            <img class="icon__img" src="<?php echo iuscanonicum_get_image_src("phone-icon.svg") ?>" alt="phone icon" />
+                        </div>
+                        <div class="contact-block__details">
+                            <h4>Numer kontaktowy</h4>
+                            <p>+48 333 333 333</p>
+                        </div>
+                    </div>
+                    <div class="contact-block__box">
+                        <div class="icon">
+                            <img class="icon__img" src="<?php echo iuscanonicum_get_image_src("clock-icon.svg") ?>" alt="clock icon" />
+                        </div>
+                        <div class="contact-block__details">
+                            <h4>Godziny otwarcia</h4>
+                            <p>Pon. - Pt. <br /> 8:00 - 16:00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
     <?php get_template_part('template-parts/footer'); ?>
 
