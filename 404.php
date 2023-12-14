64980a2ff4bb5dc324aac1339a04f829598e9991
Michał Gołda <michal.golda@hotmail.com>
2023-12-14 18:59:42 +0100

    feat: add 404 page

--- 404.php
+++ 404.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
-<main>
-    <h1>404 - Nie znaleziono strony</h1>
+<main class="not-found container">
+    <h1 class="not-found__code">404</h1>
+    <h3 class="not-found__description">Strona, której szukasz nie istnieje lub została przeniesiona.</h3>
+    <a class="button button--primary" href="<?php echo get_home_url(); ?>">
+        Strona główna
+    </a>
 </main>
 <?php get_footer(); ?>
