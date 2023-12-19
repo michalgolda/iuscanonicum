1229e6c32b089bf54f00bd7c653fa816e437c4b0
Michał Gołda <michal.golda@hotmail.com>
2023-12-19 18:24:10 +0100

    feat: use article template part

--- home.php
+++ home.php
@@ -10,17 +10,18 @@ get_header();
         if (have_posts()) :
             while (have_posts()) :
                 the_post();
-        ?>
-                <article class="article">
-                    <img class="article__cover-img" src="<?php echo iuscanonicum_get_image_src("article-cover.jpg") ?>" alt="article cover" />
-                    <div class="article__header">
-                        <p class="article__overline-heading"><?php echo get_the_category()[0]->name; ?></p>
-                        <h3 class="article__heading"><?php the_title(); ?></h3>
-                    </div>
-                    <span class="article__date"><?php echo get_the_date(); ?></span>
-                    <a class="article__ref" href="<?php the_permalink(); ?>">Przeczytaj więcej</a>
-                </article>
-        <?php
+
+                get_template_part(
+                    'template-parts/article',
+                    null,
+                    [
+                        'category-name' => get_the_category()[0]->name,
+                        'title' => get_the_title(),
+                        'date' => get_the_date(),
+                        'permalink' => get_the_permalink()
+
+                    ]
+                );
             endwhile;
         else :
             _e('Brak postów.', 'iuscnanonicum');
