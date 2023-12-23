725f80898c2924180362ea3c3b31e9f8e906ca4b
Michał Gołda <michal.golda@hotmail.com>
2023-12-23 22:06:59 +0100

    feat: use contact-block template part

--- template-contact.php
+++ template-contact.php
@@ -23,68 +23,30 @@ Template Name: Kontakt
     <main class="container">
         <?php get_template_part('template-parts/page-bar', null, ['name' => 'Kontakt']); ?>
         <?php get_template_part('template-parts/page-title', null, ['title' => 'Skontaktuj się z nami']); ?>
-        <div class="contact-block">
-            <div class="contact-block__content container">
-                <div class="contact-block__box">
-                    <div class="icon">
-                        <img class="icon__img" src="<?php echo iuscanonicum_get_image_src("email-icon.svg") ?>" alt="e-mail icon" />
+        <?php get_template_part('template-parts/contact-block'); ?>
+        <div class="content">
+            <img src="<?php echo iuscanonicum_get_image_src('google-map.jpg'); ?>" alt="google map" />
+            <div class="form-wrapper">
+                <h3>Formularz kontaktowy</h3>
+                <p>Duis fringilla interdum nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mollis feugiat odio sit amet placerat. Fusce gravida id eros sit amet laoreet. Aliquam semper augue lorem, eget accumsan ante pretium sit amet. Vestibulum vulputate dignissim porta. Proin lacus turpis, efficitur et nisi ut, commodo laoreet eros. Integer suscipit metus quis nunc semper eleifend. Fusce blandit risus ac turpis interdum consectetur. Quisque vel tristique mi.</p>
+                <form class="form">
+                    <div class="form__group">
+                        <input type="text" name="firstName" placeholder="Imię" required />
+                        <input type="text" name="lastName" placeholder="Nazwisko" required />
                     </div>
-                    <div class="contact-block__details">
-                        <h4>E-mail</h4>
-                        <p>kontakt@iuasnanonicum.pl</p>
-                    </div>
-                </div>
-                <div class="contact-block__box">
-                    <div class="icon">
-                        <img class="icon__img" src="<?php echo iuscanonicum_get_image_src("location-icon.svg") ?>" alt="location icon" />
-                    </div>
-                    <div class="contact-block__details">
-                        <h4>Lokalizacja</h4>
-                        <p>al. Beliny Prażmowskiego 9/331-514 Kraków</p>
-                    </div>
-                </div>
-                <div class="contact-block__box">
-                    <div class="icon">
-                        <img class="icon__img" src="<?php echo iuscanonicum_get_image_src("phone-icon.svg") ?>" alt="phone icon" />
-                    </div>
-                    <div class="contact-block__details">
-                        <h4>Numer kontaktowy</h4>
-                        <p>+48 333 333 333</p>
-                    </div>
-                </div>
-                <div class="contact-block__box">
-                    <div class="icon">
-                        <img class="icon__img" src="<?php echo iuscanonicum_get_image_src("clock-icon.svg") ?>" alt="clock icon" />
-                    </div>
-                    <div class="contact-block__details">
-                        <h4>Godziny otwarcia</h4>
-                        <p>Pon. - Pt. <br /> 8:00 - 16:00</p>
-                    </div>
-                </div>
-            </div>
-            <div class="content">
-                <img src="<?php echo iuscanonicum_get_image_src('google-map.jpg'); ?>" alt="google map" />
-                <div class="form-wrapper">
-                    <h3>Formularz kontaktowy</h3>
-                    <p>Duis fringilla interdum nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mollis feugiat odio sit amet placerat. Fusce gravida id eros sit amet laoreet. Aliquam semper augue lorem, eget accumsan ante pretium sit amet. Vestibulum vulputate dignissim porta. Proin lacus turpis, efficitur et nisi ut, commodo laoreet eros. Integer suscipit metus quis nunc semper eleifend. Fusce blandit risus ac turpis interdum consectetur. Quisque vel tristique mi.</p>
-                    <form class="form">
-                        <div class="form__group">
-                            <input type="text" name="firstName" placeholder="Imię" required />
-                            <input type="text" name="lastName" placeholder="Nazwisko" required />
+                    <input type="email" name="email" placeholder="E-mail" required />
+                    <input type="text" name="topic" placeholder="Temat wiadomości" required />
+                    <textarea placeholder="Treść wiadomości" name="message" required></textarea>
+                    <div class="form__group form__group--agreement">
+                        <div class="form__agreement">
+                            <input type="checkbox" name="agreement" required />
+                            <p>Wyrażam zgodę na przetwarzanie danych na potrzeby konwersacji*</p>
                         </div>
-                        <input type="email" name="email" placeholder="E-mail" required />
-                        <input type="text" name="topic" placeholder="Temat wiadomości" required />
-                        <textarea placeholder="Treść wiadomości" name="message" required></textarea>
-                        <div class="form__group form__group--agreement">
-                            <div class="form__agreement">
-                                <input type="checkbox" name="agreement" required />
-                                <p>Wyrażam zgodę na przetwarzanie danych na potrzeby konwersacji*</p>
-                            </div>
-                            <button class="button button--primary-outlined button--block" type="submit">Wyślij</button>
-                        </div>
-                    </form>
-                </div>
+                        <button class="button button--primary-outlined button--block" type="submit">Wyślij</button>
+                    </div>
+                </form>
             </div>
+        </div>
         </div>
     </main>
     <?php get_template_part('template-parts/footer'); ?>
