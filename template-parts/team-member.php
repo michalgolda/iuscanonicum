fcaba772a29887fe01840b818d6a5f7303f9f5fb
Michał Gołda <michal.golda@hotmail.com>
2023-12-23 22:49:23 +0100

    feat: load team page content using acf

---
+++ template-parts/team-member.php
@@ -0,0 +1,30 @@
+<div class="team-member">
+    <img class="team-member__img" src="<?php echo $args['image-url']; ?>" alt="team member" />
+    <div class="team-member__details">
+        <div class="team-member__heading">
+            <h3><?php echo $args['fullname']; ?></h3>
+            <p class="team-member__profession"><?php echo $args['career-title']; ?></p>
+        </div>
+        <div class="team-member__contact">
+            <div class="team-member__contact-box">
+                <div class="icon">
+                    <img class="icon__img" src="<?php echo iuscanonicum_get_image_src('email-icon.svg') ?>" alt="email icon" />
+                </div>
+                <p><?php echo $args['email']; ?></p>
+            </div>
+            <div class="team-member__contact-box">
+                <div class="icon">
+                    <img class="icon__img" src="<?php echo iuscanonicum_get_image_src('phone-icon.svg') ?>" alt="email icon" />
+                </div>
+                <p><?php echo $args['phone-number']; ?></p>
+            </div>
+            <div class="team-member__contact-box">
+                <div class="icon">
+                    <img class="icon__img" src="<?php echo iuscanonicum_get_image_src('document-icon.svg') ?>" alt="email icon" />
+                </div>
+                <a href="<?php echo $args['skill-prove-document-url']; ?>">Potwierdzenie uprawnień</a>
+            </div>
+        </div>
+        <?php echo $args['description']; ?>
+    </div>
+</div>
--- template-team.php
+++ template-team.php
@@ -24,78 +24,36 @@ Template Name: Zespół
         <?php get_template_part('template-parts/page-bar', null, ['name' => 'Zespół']); ?>
         <?php get_template_part('template-parts/page-title', null, ['title' => 'Nasz zespół']); ?>
 
-        <div class="team-member">
-            <img class="team-member__img" src="<?php echo iuscanonicum_get_image_src('team-member.jpg'); ?>" alt="team member" />
-            <div class="team-member__details">
-                <div class="team-member__heading">
-                    <h3>Joanna Zbierzchowska-Ochałek</h3>
-                    <p class="team-member__profession">Adwokat</p>
-                </div>
-                <div class="team-member__contact">
-                    <div class="team-member__contact-box">
-                        <div class="icon">
-                            <img class="icon__img" src="<?php echo iuscanonicum_get_image_src('email-icon.svg') ?>" alt="email icon" />
-                        </div>
-                        <p>joanna@iuscanonicum.pl</p>
-                    </div>
-                    <div class="team-member__contact-box">
-                        <div class="icon">
-                            <img class="icon__img" src="<?php echo iuscanonicum_get_image_src('phone-icon.svg') ?>" alt="email icon" />
-                        </div>
-                        <p>+48 333 333 333</p>
-                    </div>
-                    <div class="team-member__contact-box">
-                        <div class="icon">
-                            <img class="icon__img" src="<?php echo iuscanonicum_get_image_src('document-icon.svg') ?>" alt="email icon" />
-                        </div>
-                        <a href="#">Potwierdzenie uprawnień</a>
-                    </div>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac lorem tincidunt, venenatis sapien sed, posuere turpis. Nam ullamcorper massa nunc, vitae interdum tellus auctor nec. Vestibulum ac ullamcorper arcu, et commodo odio. Aliquam eget sodales eros. Nulla id tellus eleifend, varius lectus et, eleifend magna. Donec molestie feugiat enim, at luctus velit vulputate id. Quisque ut lobortis dui, sed maximus nisl. Aenean sit amet arcu vitae magna imperdiet ullamcorper sit amet nec tellus.</p>
-                <p>
-                    Sed faucibus lectus nec mollis ornare. Donec ac neque at ante dapibus auctor id quis metus. Nulla purus ligula, rutrum eget est nec, semper auctor augue. Quisque rutrum metus et orci interdum egestas. Quisque suscipit elit at enim vestibulum, at tempus enim porttitor. Etiam vitae maximus felis, sed condimentum mauris. Integer a lectus vitae lorem scelerisque tempus. Quisque enim erat, sagittis eget velit a, euismod lobortis metus. Praesent quis faucibus neque. Curabitur et pretium erat. Maecenas semper vestibulum nisl, eu iaculis quam dapibus at. Nulla facilisi. Curabitur molestie purus vitae libero luctus fringilla. Suspendisse varius arcu vitae diam efficitur, id laoreet metus placerat. Duis magna velit, molestie vitae augue ut, bibendum porttitor lectus.
-                </p>
-                <p>
-                    Sed faucibus lectus nec mollis ornare. Donec ac neque at ante dapibus auctor id quis metus. Nulla purus ligula, rutrum eget est nec, semper auctor augue. Quisque rutrum metus et orci interdum egestas. Quisque suscipit elit at enim vestibulum, at tempus enim porttitor. Etiam vitae maximus felis, sed condimentum mauris. Integer a lectus vitae lorem scelerisque tempus. Quisque enim erat, sagittis eget velit a, euismod lobortis metus. Praesent quis faucibus neque. Curabitur et pretium erat. Maecenas semper vestibulum nisl, eu iaculis quam dapibus at. Nulla facilisi. Curabitur molestie purus vitae libero luctus fringilla. Suspendisse varius arcu vitae diam efficitur, id laoreet metus placerat. Duis magna velit, molestie vitae augue ut, bibendum porttitor lectus.
-                </p>
-            </div>
-        </div>
-        <div class="team-member">
-            <img class="team-member__img" src="<?php echo iuscanonicum_get_image_src('team-member.jpg'); ?>" alt="team member" />
-            <div class="team-member__details">
-                <div class="team-member__heading">
-                    <h3>Joanna Zbierzchowska-Ochałek</h3>
-                    <p class="team-member__profession">Adwokat</p>
-                </div>
-                <div class="team-member__contact">
-                    <div class="team-member__contact-box">
-                        <div class="icon">
-                            <img class="icon__img" src="<?php echo iuscanonicum_get_image_src('email-icon.svg') ?>" alt="email icon" />
-                        </div>
-                        <p>joanna@iuscanonicum.pl</p>
-                    </div>
-                    <div class="team-member__contact-box">
-                        <div class="icon">
-                            <img class="icon__img" src="<?php echo iuscanonicum_get_image_src('phone-icon.svg') ?>" alt="email icon" />
-                        </div>
-                        <p>+48 333 333 333</p>
-                    </div>
-                    <div class="team-member__contact-box">
-                        <div class="icon">
-                            <img class="icon__img" src="<?php echo iuscanonicum_get_image_src('document-icon.svg') ?>" alt="email icon" />
-                        </div>
-                        <a href="#">Potwierdzenie uprawnień</a>
-                    </div>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac lorem tincidunt, venenatis sapien sed, posuere turpis. Nam ullamcorper massa nunc, vitae interdum tellus auctor nec. Vestibulum ac ullamcorper arcu, et commodo odio. Aliquam eget sodales eros. Nulla id tellus eleifend, varius lectus et, eleifend magna. Donec molestie feugiat enim, at luctus velit vulputate id. Quisque ut lobortis dui, sed maximus nisl. Aenean sit amet arcu vitae magna imperdiet ullamcorper sit amet nec tellus.</p>
-                <p>
-                    Sed faucibus lectus nec mollis ornare. Donec ac neque at ante dapibus auctor id quis metus. Nulla purus ligula, rutrum eget est nec, semper auctor augue. Quisque rutrum metus et orci interdum egestas. Quisque suscipit elit at enim vestibulum, at tempus enim porttitor. Etiam vitae maximus felis, sed condimentum mauris. Integer a lectus vitae lorem scelerisque tempus. Quisque enim erat, sagittis eget velit a, euismod lobortis metus. Praesent quis faucibus neque. Curabitur et pretium erat. Maecenas semper vestibulum nisl, eu iaculis quam dapibus at. Nulla facilisi. Curabitur molestie purus vitae libero luctus fringilla. Suspendisse varius arcu vitae diam efficitur, id laoreet metus placerat. Duis magna velit, molestie vitae augue ut, bibendum porttitor lectus.
-                </p>
-                <p>
-                    Sed faucibus lectus nec mollis ornare. Donec ac neque at ante dapibus auctor id quis metus. Nulla purus ligula, rutrum eget est nec, semper auctor augue. Quisque rutrum metus et orci interdum egestas. Quisque suscipit elit at enim vestibulum, at tempus enim porttitor. Etiam vitae maximus felis, sed condimentum mauris. Integer a lectus vitae lorem scelerisque tempus. Quisque enim erat, sagittis eget velit a, euismod lobortis metus. Praesent quis faucibus neque. Curabitur et pretium erat. Maecenas semper vestibulum nisl, eu iaculis quam dapibus at. Nulla facilisi. Curabitur molestie purus vitae libero luctus fringilla. Suspendisse varius arcu vitae diam efficitur, id laoreet metus placerat. Duis magna velit, molestie vitae augue ut, bibendum porttitor lectus.
-                </p>
-            </div>
-        </div>
+
+        <?php
+        wp_reset_query();
+
+        $query_args = [
+            'post_type' => 'team-member',
+            'post_status' => 'private'
+        ];
+
+        $query = new WP_Query($query_args);
+        while ($query->have_posts()) :
+            $query->the_post();
+
+            get_template_part(
+                'template-parts/team-member',
+                null,
+                [
+                    'fullname' => iuscanonicum_remove_prefix_from_post_title(get_the_title()),
+                    'description' => get_the_content(),
+                    'career-title' => get_field('career-title'),
+                    'email' => get_field('email'),
+                    'phone-number' => get_field('phone-number'),
+                    'skill-prove-document-url' => get_field('skill-prove-document-url'),
+                    'image-url' => get_field('image')
+                ]
+            );
+        endwhile;
+
+        wp_reset_postdata();
+        ?>
     </main>
     <?php get_template_part('template-parts/footer'); ?>
 
