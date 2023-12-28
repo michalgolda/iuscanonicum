ed5b505cfd240abcb29e7097eae391f14a973b5d
Michał Gołda <michal.golda@hotmail.com>
2023-12-28 18:01:28 +0100

    feat: create template for post service type

---
+++ single-service.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<main class="container">
+    <?php get_template_part('template-parts/page-bar', null, ['name' => 'Usługi']); ?>
+    <?php get_template_part('template-parts/page-title', null, ['title' => get_the_title()]); ?>
+
+    <?php the_content(); ?>
+
+    <?php get_template_part('template-parts/latest-articles'); ?>
+</main>
+
+<?php get_footer(); ?>
