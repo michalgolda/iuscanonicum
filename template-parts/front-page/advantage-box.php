4c0713364a92e81bb9bf6924c56a5f9de3df7213
= <=>
2023-12-31 20:18:04 +0100

    feat: add alt attr for img tag

--- template-parts/front-page/advantage-box.php
+++ template-parts/front-page/advantage-box.php
@@ -1,6 +1,6 @@
 <div class="advantages-block__box">
     <div class="icon">
-        <img class="icon__img" src="<?php echo $args['icon-src']; ?>" alt="clock icon" />
+        <img class="icon__img" src="<?php echo $args['icon-src']; ?>" alt="<?php echo $args['name']; ?>" />
     </div>
     <div class="advantages-block__details">
         <h3><?php echo $args['name']; ?></h3>
