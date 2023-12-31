38ed77dc174b8081466e12548772f94e16cb0e68
= <=>
2023-12-31 16:47:53 +0100

    refactor: move team page related parts to team sub folder

--- templates/team.php
+++ templates/team.php
@@ -2,62 +2,43 @@
 /*
 Template Name: Zespół
 */
+
+get_header();
 ?>
 
-<!DOCTYPE html>
-<html lang="pl">
+<main class="container">
+    <?php get_template_part('template-parts/page-bar', null, ['name' => 'Zespół']); ?>
+    <?php get_template_part('template-parts/page-title', null, ['title' => 'Nasz zespół']); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    wp_reset_query();
 
-    <?php wp_head(); ?>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    <link href="https://api.fontshare.com/v2/css?f[]=amulya@400,500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-</head>
+    $query_args = [
+        'post_type' => 'team-member',
+        'post_status' => 'private'
+    ];
 
-<body class="team-page">
-    <?php get_template_part('template-parts/header'); ?>
-    <main class="container">
-        <?php get_template_part('template-parts/page-bar', null, ['name' => 'Zespół']); ?>
-        <?php get_template_part('template-parts/page-title', null, ['title' => 'Nasz zespół']); ?>
+    $query = new WP_Query($query_args);
+    while ($query->have_posts()) :
+        $query->the_post();
 
+        get_template_part(
+            'template-parts/team/team-member',
+            null,
+            [
+                'fullname' => iuscanonicum_remove_prefix_from_post_title(get_the_title()),
+                'description' => get_the_content(),
+                'career-title' => get_field('career-title'),
+                'email' => get_field('email'),
+                'phone-number' => get_field('phone-number'),
+                'skill-prove-document-url' => get_field('skill-prove-document-url'),
+                'image-url' => get_field('image')
+            ]
+        );
+    endwhile;
 
-        <?php
-        wp_reset_query();
+    wp_reset_postdata();
+    ?>
+</main>
 
-        $query_args = [
-            'post_type' => 'team-member',
-            'post_status' => 'private'
-        ];
-
-        $query = new WP_Query($query_args);
-        while ($query->have_posts()) :
-            $query->the_post();
-
-            get_template_part(
-                'template-parts/team-member',
-                null,
-                [
-                    'fullname' => iuscanonicum_remove_prefix_from_post_title(get_the_title()),
-                    'description' => get_the_content(),
-                    'career-title' => get_field('career-title'),
-                    'email' => get_field('email'),
-                    'phone-number' => get_field('phone-number'),
-                    'skill-prove-document-url' => get_field('skill-prove-document-url'),
-                    'image-url' => get_field('image')
-                ]
-            );
-        endwhile;
-
-        wp_reset_postdata();
-        ?>
-    </main>
-    <?php get_template_part('template-parts/footer'); ?>
-
-    <?php wp_footer(); ?>
-</body>
-
-</html>
+<?php get_footer(); ?>
