8b40e1c0ad7eeb8024ccbe3b19051cff0463183d
= <=>
2023-12-31 15:04:06 +0100

    feat: use dynamic_sidebar for text editing

--- front-page.php
+++ front-page.php
@@ -7,10 +7,10 @@
         <img src="<?php echo iuscanonicum_get_image_src('scroll-down-arrow.svg'); ?>" alt="scroll down arrow" />
     </a>
     <section id="welcome" class="section">
-        <h2 class="section__heading">Witamy w naszej kancelarii</h2>
+        <?php dynamic_sidebar('iuscanonicum-front-page-welcome-section-heading'); ?>
         <div class="welcome-block">
             <div class="container">
-                <p>Duis felis nisi, vulputate quis accumsan non, finibus ac sapien. Etiam pharetra faucibus quam, ac auctor turpis commodo vitae. Integer condimentum ex eget est cursus porta. Vivamus posuere ante in lobortis tristique. Mauris ipsum dui, aliquam at dapibus quis, sagittis et dui. Sed pharetra elit arcu, nec luctus purus ultricies id. Nam imperdiet semper bibendum. Aenean bibendum ex sed ligula ultricies semper. Ut ante velit, vestibulum id elementum quis, mollis vitae nulla. Suspendisse potenti. In lorem turpis, auctor vel ultricies at, posuere id ipsum. Vestibulum vulputate ipsum arcu, sed laoreet metus efficitur in.</p>
+                <?php dynamic_sidebar('iuscanonicum-front-page-welcome-section-body'); ?>
             </div>
         </div>
         <?php get_template_part('template-parts/contact-block'); ?>
@@ -19,10 +19,10 @@
     <?php get_template_part('template-parts/advantages'); ?>
     <section id="weCanHelpYou" class="section">
         <div class="weCanHelpYou-block">
-            <h2 class="section__heading">Dowiedz się w jaki sposób możemy Ci pomóc</h2>
+            <?php dynamic_sidebar('iuscanonicum-front-page-we-can-help-you-section-heading'); ?>
             <div class="weCanHelpYou-block__details container">
-                <p>Etiam consectetur placerat nisl sed iaculis. Cras feugiat pretium feugiat. Suspendisse potenti. Donec nec metus suscipit, fringilla nibh quis, scelerisque urna. Integer ac enim ipsum. Donec et molestie magna. Phasellus dictum placerat diam, quis blandit dolor vehicula eu. Nullam vel urna a sem sodales tincidunt sed ut dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc in enim eleifend, feugiat urna eu, molestie nisl. Quisque ac lobortis ipsum. Mauris interdum nisi dui, a lobortis augue malesuada vitae.</p>
-                <a class="button button--primary" href="#">Skontaktuj się</a>
+                <?php dynamic_sidebar('iuscanonicum-front-pagewe-can-help-you-section-body'); ?>
+                <a class="button button--primary" href="/kontakt">Skontaktuj się</a>
             </div>
         </div>
     </section>
