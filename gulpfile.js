5ef048ab0b503abe5ba4d9d2ec837504681ec5a6
= <=>
2024-01-01 15:32:31 +0100

    build: add gulp-clean-css package

--- gulpfile.js
+++ gulpfile.js
@@ -2,36 +2,24 @@ const gulp = require("gulp");
 const clean = require("gulp-clean");
 const sass = require("gulp-sass")(require("sass"));
 const rename = require("gulp-rename");
+const cleanCss = require("gulp-clean-css");
 
 function cleanStyleFile() {
   return gulp.src("style.css", { allowEmpty: true, read: false }).pipe(clean());
 }
 
-function compileStyles(minify = true) {
+function compileStyles() {
   return gulp
     .src("assets/styles/main.scss")
-    .pipe(
-      sass({
-        outputStyle: minify ? "compressed" : "expanded",
-        file: "style",
-      }).on("error", sass.logError)
-    )
+    .pipe(sass({ file: "style" }).on("error", sass.logError))
     .pipe(rename("style.css"))
+    .pipe(cleanCss({ level: 2 }))
     .pipe(gulp.dest("./"));
 }
 
-const compileStylesWithoutCompression = () => compileStyles(false);
-
 function watch() {
-  gulp.watch(
-    "assets/styles/**/*.scss",
-    gulp.series(compileStylesWithoutCompression)
-  );
+  gulp.watch("assets/styles/**/*.scss", gulp.series(compileStyles));
 }
 
 exports.build = gulp.series(cleanStyleFile, compileStyles);
-exports.dev = gulp.series(
-  cleanStyleFile,
-  compileStylesWithoutCompression,
-  watch
-);
+exports.dev = gulp.series(cleanStyleFile, compileStyles, watch);
