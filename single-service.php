a470e2f0f5bdc5e76f422db3022fa74c0bdb1e57
= <=>
2024-01-02 19:28:49 +0100

    feat: wrap the_content() with div

--- single-service.php
+++ single-service.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
 
-<main class="container post-content">
+<main class="container">
     <?php get_template_part('template-parts/page-bar', null, ['name' => 'Usługi']); ?>
     <?php get_template_part('template-parts/page-title', null, ['title' => get_the_title()]); ?>
 
-    <?php the_content(); ?>
+    <div class="post-content">
+        <?php the_content(); ?>
+    </div>
 
     <?php get_template_part('template-parts/latest-articles'); ?>
 </main>
--- single.php
+++ single.php
@@ -4,7 +4,9 @@
     <?php get_template_part('template-parts/page-bar', null, ['name' => 'Blog']); ?>
     <?php get_template_part('template-parts/page-title', null, ['title' => get_the_title()]); ?>
 
-    <?php the_content(); ?>
+    <div class="post-content">
+        <?php the_content(); ?>
+    </div>
 
     <?php get_template_part('template-parts/other-articles'); ?>
 </main>
