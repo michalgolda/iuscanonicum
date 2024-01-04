83144d63acced05dd41652c5e2522197a0ced8bf
= <=>
2024-01-04 19:28:20 +0100

    feat: change viewport meta content

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0" />
 
     <?php wp_head(); ?>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
