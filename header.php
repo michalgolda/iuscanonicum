b9d813727cdb36304ca848feff6eaa30268686ae
= <=>
2024-03-15 12:38:37 +0100

    fix: uses third-party cookies

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
 
     <?php wp_head(); ?>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    <link href="https://api.fontshare.com/v2/css?f[]=amulya@400,500&display=swap" rel="stylesheet">
 </head>
 
 <body <?php body_class(); ?>>
